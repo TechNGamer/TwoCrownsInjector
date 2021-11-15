dcc5ed994d1b2496586a886820dc75bfd62cc17a
Jeff Crow <jeffwcrow@gmail.com>
2021-11-15 13:18:37 -0600

    Added some comments to help better understand what is going on and what things are for.

--- BasicModLoader/LoaderEntry.cs
+++ BasicModLoader/LoaderEntry.cs
@@ -7,6 +7,7 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace BasicModLoader {
+	[SuppressMessage( "ReSharper", "MemberCanBePrivate.Global", Justification = "Some mods might need this information.")]
 	public static class LoaderEntry {
 		public static bool Loaded { get; private set; }
 
@@ -18,17 +19,21 @@ namespace BasicModLoader {
 				// ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
 				switch ( Application.platform ) {
 					case RuntimePlatform.OSXPlayer:
+						// The reason the mod folder is different for macOS is that it is an App Bundle, and I don't have a mac, so inside the bundle it goes.
 						folder = Path.Combine( Application.dataPath, "Mods" );
 						break;
 					case RuntimePlatform.OSXEditor:
 					case RuntimePlatform.LinuxEditor:
 					case RuntimePlatform.WindowsEditor:
+						// Would be interesting for this to be running inside an editor, but since that shouldn't happen, an error will arise.
 						throw new PlatformNotSupportedException( "How/Why are you using this on an editor?" );
 					case RuntimePlatform.WindowsPlayer:
 					case RuntimePlatform.LinuxPlayer:
+						// Since it will be in the Data folder, we go up one and then into the mods folder.
 						folder = Path.Combine( Application.dataPath, "..", "Mods" );
 						break;
 					default:
+						// This loader doesn't support other platforms.
 						throw new PlatformNotSupportedException();
 				}
 
@@ -43,10 +48,17 @@ namespace BasicModLoader {
 
 		[SuppressMessage( "ReSharper", "UnusedMember.Global", Justification = "This is the entry point for loading mods." )]
 		public static void BeginModLoading() {
+			// It is has ran once, doesn't need to run again.
+			if ( Loaded ) {
+				return;
+			}
+			
+			// Loops through all the mod folders so it can load them.
 			foreach ( var modFolders in Directory.EnumerateDirectories( ModFolder, "*", SearchOption.TopDirectoryOnly ) ) {
 				LoadAssemblies( modFolders );
 			}
 
+			// The flag for being loaded will be set to true, as to prevent this method from running this again.
 			Loaded = true;
 		}
 
@@ -59,6 +71,7 @@ namespace BasicModLoader {
 				return;
 			}
 
+			// This section loads every assembly that is in the Assemblies folder.
 			foreach ( var assemblyFile in Directory.EnumerateFiles( asmFolder, "*.dll", SearchOption.TopDirectoryOnly ) ) {
 				var assembly = Assembly.LoadFile( assemblyFile );
 
@@ -75,10 +88,11 @@ namespace BasicModLoader {
 			Debug.Log( $"All done loading Assemblies in `{asmFolder}`." );
 		}
 
+		// This section will look through each Assembly and run every static constructor that has the `RunStaticConstructorOnLoad`
 		private static void CheckAndRun( Assembly asm ) {
-			// This is the fun part where Reflection is going to be used to locate all the classes that have the custom attribute `LoadStaticConstructorOnLoad`.
+			// This is the fun part where Reflection is going to be used to locate all the classes that have the custom attribute `RunStaticConstructorOnLoad` and run their static constructor.
 			foreach ( var type in asm.GetTypes() ) {
-				if ( type.GetCustomAttributes( false ).FirstOrDefault( a => a is LoadStaticConstructorOnLoad ) == null ) {
+				if ( type.GetCustomAttributes( false ).FirstOrDefault( a => a is RunStaticConstructorOnLoad ) == null ) {
 					continue;
 				}
 
--- BasicModLoader/RunStaticConstructorOnLoad.cs
+++ BasicModLoader/RunStaticConstructorOnLoad.cs
@@ -2,6 +2,9 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 
 namespace BasicModLoader {
+	/// <summary>
+	/// Signals to the Mod loader that it should run the static constructor before proceeding to the next assembly.
+	/// </summary>
 	[AttributeUsage( AttributeTargets.Class, Inherited = false )]
 	[SuppressMessage( "ReSharper", "ClassNeverInstantiated.Global" )]
 	public class RunStaticConstructorOnLoad : Attribute {
