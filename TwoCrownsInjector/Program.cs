0697b014af0df98e7afc4cbf20bcf5ae6a0fe4b2
Jeff Crow <jeffwcrow@gmail.com>
2021-11-15 13:00:56 -0600

    Added some comments to help remember what each part of the code does. Also changed some things to make it more reasonable and also removed useless pieces of code.

--- TwoCrownsInjector/Program.cs
+++ TwoCrownsInjector/Program.cs
@@ -13,6 +13,9 @@ namespace TwoCrownsInjector {
 
 		private static readonly Exception NOT_FOUND_EXCEPTION = new FileNotFoundException( "Attempted to grab path for mod loader, but got nothing." );
 
+		/* As the name says, it assumes the default steam location of where the game might be.
+		 * So for Windows, the Program Files (x86) location. For macOS, the Library/Application Support location.
+		 * For Linux, it assumes the .steam folder in the user's home partition. */
 		private static string AssumeLocation {
 			get {
 				if ( RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) {
@@ -33,6 +36,11 @@ namespace TwoCrownsInjector {
 			}
 		}
 
+		/* This guesses, but is an accurate assumption, of where the game has it's save located at.
+		 * Unity has the location of `Application.persistantData` located at the locations that are
+		 * returned by this property. The reason I am using a property is for the fact that is is basic,
+		 * doesn't process any information from the class itself, returns a core type, and that it would
+		 * look weird to make it a method. */
 		private static string GameSaveLocation {
 			get {
 				if ( RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) {
@@ -53,80 +61,86 @@ namespace TwoCrownsInjector {
 			}
 		}
 
-		private static bool ForceRePatch { get; set; }
+		// These guys are fields since they are simply storing data, no processing and not presenting to other classes/objects.
+		private static bool forceRePatch;
 
-		private static string Location { get; set; } = AssumeLocation;
+		private static string location = AssumeLocation;
 
 		private static void Main( string[] args ) {
+			// Assume that the user wants to interact with the program.
 			var interactive = true;
 
-			#if DEBUG
-			Console.WriteLine( $"My working directory is `{Environment.CurrentDirectory}`." );
-			#endif
-
+			// Processes the arguments. The only supported arguments is --game-location (with the next being a path), --no-interactive, and -f/--force for force repatching.
 			for ( var i = 0; i < args.Length; ++i ) {
 				if ( args[i] == "--game-location" ) {
-					Location = args[++i];
+					location = args[++i];
 				} else if ( args[i] == "--no-interactive" ) {
 					interactive = false;
 				} else if ( args[i] == "-f" || args[i] == "--force" ) {
-					ForceRePatch = true;
+					forceRePatch = true;
 				}
 			}
 
 			if ( interactive ) {
 				Interactive();
 			} else {
-				if ( !Directory.Exists( Location ) ) {
+				if ( !Directory.Exists( location ) ) {
 					throw new DirectoryNotFoundException( "The provided directory does not exist. Please make sure you pointed the program to the proper place." );
 				}
 
-				StartPatching( Path.Combine( Location, "KingdomTwoCrowns_Data", "Managed" ) );
+				StartPatching( Path.Combine( location, "KingdomTwoCrowns_Data", "Managed" ) );
 			}
 		}
 
 		private static void Interactive() {
-			var loc = Location;
+			var loc = location;
 
 			// This section tells the user what this program is, and to also back up their saves.
 			Console.WriteLine( "Welcome to the Kingdom: Two Crowns Mod Patcher." );
-
 			Console.ForegroundColor = ConsoleColor.Red;
-
 			Console.WriteLine( $"Back up your save files before doing this.\nLocation is `{GameSaveLocation}`".Blinking() );
-
 			Console.ResetColor();
-
 			Console.WriteLine( "Press any key to continue. . ." );
-
 			Console.ReadKey();
 
+			// Keeps asking the user to input a valid path.
 			while ( string.IsNullOrWhiteSpace( loc ) || !Directory.Exists( loc ) ) {
 				Console.WriteLine( $"Please enter the full location of the game. Example: {AssumeLocation}\n" );
 
 				loc = Console.ReadLine();
 			}
-
+			
 			Debug.Assert( loc != null, nameof( loc ) + " != null" );
 
+			/* Where the magic happens where it will start patching a Unity library.
+			 * The reason for patching a Unity library instead of the `Assembly-CSharp.dll`
+			 * is because there are no methods that are guaranteed to be called on the start
+			 * of the game. However, if we patch `Application`, which is a frequently used class,
+			 * then there is a high chance of our patch being applied to and working. Look at
+			 * the method to see how this is accomplished. */
 			StartPatching( Path.Combine( loc, "KingdomTwoCrowns_Data", "Managed" ) );
 
-			Console.WriteLine( "Injecting complete, have fun modding." );
+			Console.WriteLine( "Injecting complete, have fun modding.\nPress any key to continue. . ." );
 
 			Console.ReadKey();
 		}
 
 		private static void StartPatching( string managedLocation ) {
+			// This section is grabbing everything that is needed for patching.
 			var unityCore  = Path.Combine( managedLocation, "UnityEngine.CoreModule.dll" );
 			var modLibrary = GetModLibraryPath();
 			var modsFolder = Path.Combine( managedLocation, "..", "..", "Mods" );
 
+			// Checks to see if the mod directory exists and creates one if it isn't. Why? To make sure it is there.
 			if ( !Directory.Exists( modsFolder ) ) {
 				_ = Directory.CreateDirectory( modsFolder );
 			}
 
 			Console.WriteLine( "Coping needed libraries over." );
 
+			/* The mod loader also bundles Harmony with it, so that is why 2 assemblies are getting copied.
+			 * Harmony allows mods to patch objects at runtime without being destructive, like we're doing.
+			 * Since we do not have any openings to begin with, we have to be destructive. */
 			File.Copy( modLibrary, Path.Combine( managedLocation, MOD_LIBRARY_NAME ), true );
 			File.Copy( GetHarmonyPath(), Path.Combine( managedLocation, HARMONY_LIBRARY_NAME ), true );
 
@@ -134,15 +148,17 @@ namespace TwoCrownsInjector {
 
 			var resolver = new DefaultAssemblyResolver();
 
+			// We want the resolver to look at the managed folder of the game, since that is where all the managed DLL's are at.
 			resolver.AddSearchDirectory( managedLocation );
-
+			
+			// This section just opens UnityEngine.CoreModule.dll and also looks to see if it is already patched.
 			var readerParams = new ReaderParameters() {
 				AssemblyResolver = resolver
 			};
 			var asmStream = new FileStream( unityCore, FileMode.Open );
 			var unityDef  = AssemblyDefinition.ReadAssembly( asmStream, readerParams );
 
-			if ( !ForceRePatch && IsAlreadyInjected( unityDef ) ) {
+			if ( !forceRePatch && IsAlreadyInjected( unityDef ) ) {
 				unityDef.Dispose();
 				asmStream.Dispose();
 
@@ -153,6 +169,13 @@ namespace TwoCrownsInjector {
 
 			Console.WriteLine( "Patching UnityEngine.CoreModule.dll." );
 
+			/* This section is where it is fun, and also complicated. It first creates a new AssemblyDefinition for
+			 * the mod loader assembly. It then locates the class called `LoaderEntry`, then the method `BeginModLoading`
+			 * where, as the name implies, it begins loading of the mods. Once it has finished that, we then create a
+			 * MethodReference from the Unity AssemblyDefinition to the method we want to call. We then look for the
+			 * type `Application` inside `UnityEngine.CoreModule.dll`. We also need a TypeReference which would be the
+			 * return type. But since we are going to be calling this method from the static constructor, it's return
+			 * type is going to be void. Lastly for this giant variable block, we get the IL Processor. */
 			var injected = AssemblyDefinition.ReadAssembly( MOD_LIBRARY_NAME, readerParams );
 			var injectMethod = injected.MainModule.Types.First( t => t.Name == "LoaderEntry" )
 				.Methods.First( m => m.Name == "BeginModLoading" );
@@ -166,9 +189,13 @@ namespace TwoCrownsInjector {
 			);
 			var ilProc = classCon.Body.GetILProcessor();
 
+			// We inject the call operand code into the static constructor, then a return since we don't want it to do things beyond the method.
 			ilProc.Append( ilProc.Create( OpCodes.Call, injectRef ) );
 			ilProc.Append( ilProc.Create( OpCodes.Ret ) );
 
+			/* We then write the newly created codes out to the list of methods of that class.
+			 * Write the changes we made to the assembly, then dispose of everything that needs disposing of.
+			 * And now this method is done. */
 			application.Methods.Add( classCon );
 
 			unityDef.Write();
@@ -203,6 +230,7 @@ namespace TwoCrownsInjector {
 			return modLoader.FullName;
 		}
 
+		// This method assumes that the program's Current Directory is the same as where it's assemblies are at.
 		private static string GetAssemblyPath() {
 			var myAsm         = Environment.CurrentDirectory;
 			var processModule = Process.GetCurrentProcess().MainModule;
@@ -224,6 +252,7 @@ namespace TwoCrownsInjector {
 			return myAsm;
 		}
 
+		// Little trick I learned to make a line on the terminal blink. It doesn't seem to work on Windows.
 		private static string Blinking( this string str ) {
 			if ( RuntimeInformation.IsOSPlatform( OSPlatform.Windows ) ) {
 				return str;
